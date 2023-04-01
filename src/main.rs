e4f71330f9115e2dfc5fe0fdb488c637fa4c0bf3
Matthew Salsamendi <matthewsalsamendi@gmail.com>
2023-03-31 20:49:53 -0700

    Norm values

--- src/main.rs
+++ src/main.rs
@@ -79,10 +79,10 @@ async fn main() {
     tokio::task::spawn(async move {
         let mut rng = StdRng::from_entropy();
 
-        let mut tick_interval = tokio::time::interval(std::time::Duration::from_micros(500));
+        let mut tick_interval = tokio::time::interval(std::time::Duration::from_micros(5_000));
 
         const MAX_CONCURRENT_GAMES: usize = 10;
-        const MAX_STORED_GAMES: usize = 10_000;
+        const MAX_STORED_GAMES: usize = 100_000;
         let mut boards: Vec<Board> = vec![];
         let mut total_boards_created: usize = 0;
         loop {
